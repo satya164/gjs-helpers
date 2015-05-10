ce5d122553fa32fee35062ae2651deda65ab935e
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-10 10:02:12 +0530

    Use GLib instead of Mainloop

--- src/timing.js
+++ src/timing.js
@@ -1,19 +1,19 @@
 // Timing events from - https://github.com/optimisme/gjs-examples
 
-const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
 
 const setTimeout = (func, millis) => {
-    return Mainloop.timeout_add(millis, () => {
+    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, millis, () => {
         func();
 
         return false; // Don't repeat
     }, null);
 };
 
-const clearTimeout = id => Mainloop.source_remove(id);
+const clearTimeout = id => GLib.Source.remove(id);
 
 const setInterval = (func, millis) => {
-    let id = Mainloop.timeout_add(millis, () => {
+    let id = GLib.timeout_add(GLib.PRIORITY_DEFAULT, millis, () => {
         func();
 
         return true; // Repeat
@@ -22,4 +22,4 @@ const setInterval = (func, millis) => {
     return id;
 };
 
-const clearInterval = id => Mainloop.source_remove(id);
+const clearInterval = id => GLib.Source.remove(id);
